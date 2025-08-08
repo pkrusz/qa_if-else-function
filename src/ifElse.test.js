92a881ce99341964d82d637b5c2efd4bc9c1f7ad
Piotr Kruszelnicki <arbuzzz1337@gmail.com>
2025-08-08 18:26:34 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -8,8 +8,9 @@ describe('ifElse', () => {
     const first = jest.fn();
     const second = jest.fn();
 
-    ifElse(condition, first, second);
+    const result = ifElse(condition, first, second);
 
+    expect(result).toBeUndefined();
     expect(condition).toHaveBeenCalledTimes(1);
     expect(first).toHaveBeenCalledTimes(1);
     expect(second).not.toHaveBeenCalled();
@@ -20,14 +21,15 @@ describe('ifElse', () => {
     const first = jest.fn();
     const second = jest.fn();
 
-    ifElse(condition, first, second);
+    const result = ifElse(condition, first, second);
 
+    expect(result).toBeUndefined();
     expect(condition).toHaveBeenCalledTimes(1);
     expect(first).not.toHaveBeenCalled();
     expect(second).toHaveBeenCalledTimes(1);
   });
 
-  it('should pass no arguments to callbacks', () => {
+  it('should pass no arguments to callbacks when condition is true', () => {
     const condition = jest.fn(() => true);
     const first = jest.fn();
     const second = jest.fn();
@@ -37,5 +39,17 @@ describe('ifElse', () => {
     expect(condition).toHaveBeenCalledWith();
     expect(first).toHaveBeenCalledWith();
     expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should pass no arguments to callbacks when condition is false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledWith();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledWith();
   });
 });
